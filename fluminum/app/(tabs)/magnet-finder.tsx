bb8816f90a60d06c6eca611111681c5950ba3202
Jannik Pulfer <jpu140816@stud.gibb.ch>
2024-09-09 14:06:34 +0200

    Add gitignore for idea folder and print absolute value of magnet sensor

--- fluminum/app/(tabs)/magnet-finder.tsx
+++ fluminum/app/(tabs)/magnet-finder.tsx
@@ -1,24 +1,24 @@
-import { Magnetometer, type MagnetometerMeasurement } from "expo-sensors";
+import { Magnetometer } from "expo-sensors";
 import * as React from "react";
 import { View } from "react-native";
 import { Text } from "~/components/ui/text";
 
 export default function Screen() {
-  const [datas, setDatas] = React.useState<MagnetometerMeasurement[]>([]);
+  const [magnetometerValue, setMagnetometerValue] = React.useState<Number>(0);
 
   React.useEffect(() => {
-    const l = Magnetometer.addListener((e) =>
-      setDatas((d) => [...d, e].slice(-5)),
+    const listener = Magnetometer.addListener((e) =>
+        setMagnetometerValue(e.x + e.y + e.z)
     );
 
     Magnetometer.setUpdateInterval(1);
 
-    () => l.remove();
+    return () => listener.remove();
   }, []);
 
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Text className="text-muted-foreground">{JSON.stringify(datas)}</Text>
+      <Text className="text-muted-foreground">{magnetometerValue.toString()}</Text>
     </View>
   );
 }
