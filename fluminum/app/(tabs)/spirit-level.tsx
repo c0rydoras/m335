b7445fb106399055ff4a481eb62eb9a0bb9ba4ac
MarklinH0 <74905481+MarklinH0@users.noreply.github.com>
2024-09-13 15:28:52 +0200

    spirit-level (#7)
    * add sprit-level
    
    * add set referance point setter
    
    * used more memos
    
    * add test for spirit-level.tsx
    
    * add snapshot for test
    
    * fix typos and remove console log
    
    * chore: formatted
    
    * remove unused import from spirit-level-test
    
    * remove unused import from spirit-level tab
    
    ---------
    
    Co-authored-by: e544631 <andrin.gurtner2@sbb.ch>
    Co-authored-by: Arthur Deierlein <info@c0rydoras.dev>
    Co-authored-by: Arthur <110528300+c0rydoras@users.noreply.github.com>

--- fluminum/app/(tabs)/spirit-level.tsx
+++ fluminum/app/(tabs)/spirit-level.tsx
@@ -1,21 +1,88 @@
-import * as React from "react";
 import { View } from "react-native";
-import { Text } from "~/components/ui/text";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { DeviceMotion } from "expo-sensors";
+import Svg, { Text, Line, Polygon } from "react-native-svg";
+import { Text as UiText } from "~/components/ui/text";
+import { Button } from "~/components/ui/button";
 
+export function calculatePoints(topBottomDeg: number, leftRightDeg: number) {
+  const middlePoint = "50,50";
+  const topY = 50 - (topBottomDeg / 90) * 50;
+  const topX1 = topY;
+  const topX2 = 100 - topY;
+  const pointsTop = middlePoint + ` ${topX1},${topY} ${topX2},${topY}`;
+  const leftX = 50 - (leftRightDeg / 90) * 50;
+  const leftY1 = leftX;
+  const leftY2 = 100 - leftX;
+  const pointsLeft = middlePoint + ` ${leftX},${leftY1} ${leftX},${leftY2}`;
+  return { pointsTop, pointsLeft };
+}
 export default function Screen() {
+  const [{ beta, gamma }, setData] = useState({
+    beta: 0,
+    gamma: 0,
+  });
+
+  const betaOfset = useRef(0);
+  const gammaOfset = useRef(0);
+  useEffect(() => {
+    DeviceMotion.setUpdateInterval(25);
+    const listener = DeviceMotion.addListener((data) => {
+      if (data && data.rotation) {
+        const localData = { beta: 0, gamma: 0 };
+        localData.beta = data.rotation.beta - betaOfset.current;
+        localData.gamma = data.rotation.gamma - gammaOfset.current;
+        setData(localData);
+      }
+    });
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  const { pointsTop, pointsLeft } = useMemo(
+    () => calculatePoints((180 / Math.PI) * beta, (180 / Math.PI) * gamma),
+    [beta, gamma],
+  );
+
+  const { betaDisplay, gammaDisplay } = useMemo(
+    () => ({
+      betaDisplay: (180 / Math.PI) * beta,
+      gammaDisplay: (180 / Math.PI) * gamma,
+    }),
+    [beta, gamma],
+  );
+
   return (
-    <View className="p-6 bg-secondary/30 relative h-72 flex gap-5 m-10">
-      <View className="absolute top-0 inset-x-[50%]">
-        <Text>Top</Text>
-      </View>
-      <View className="absolute right-0 inset-y-[50%]">
-        <Text>Right</Text>
-      </View>
-      <View className="absolute left-0 inset-y-[50%]">
-        <Text>Left</Text>
-      </View>
-      <View className="absolute bottom-0 inset-x-[50%]">
-        <Text>Bottom</Text>
+    <View className="flex-row flex justify-center w-full">
+      <View className="w-[95%] h-[90%] flex items-center justify-center flex-col">
+        <Svg viewBox="0 0 100 100">
+          <Polygon fill="#FFE500" points={pointsTop} />
+          <Polygon fill="#FFE500" points={pointsLeft} />
+          <Line stroke={"black"} x1={0} y1={0} x2={100} y2={100} />
+          <Line stroke={"black"} x1={100} y1={0} x2={0} y2={100} />
+          <Text textAnchor="middle" y="20" x="50">
+            {betaDisplay.toFixed(0) + "°"}
+          </Text>
+          <Text textAnchor="middle" y="80" x="50">
+            {(-betaDisplay).toFixed(0) + "°"}
+          </Text>
+          <Text textAnchor="middle" y="50" x="20">
+            {gammaDisplay.toFixed(0) + "°"}
+          </Text>
+          <Text textAnchor="middle" y="50" x="80">
+            {(-gammaDisplay).toFixed(0) + "°"}
+          </Text>
+        </Svg>
+        <Button
+          onPress={() => {
+            betaOfset.current = beta + betaOfset.current;
+            gammaOfset.current = gamma + gammaOfset.current;
+          }}
+          className="bottom-0 bg-[#FFE500] text-black w-2/3"
+        >
+          <UiText className="text-black">Referenzpunkt Setzen</UiText>
+        </Button>
       </View>
     </View>
   );
