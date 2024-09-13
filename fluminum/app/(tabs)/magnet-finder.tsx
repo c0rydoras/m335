62557bd408a9b79847c91a548d2364b31b5122a9
Jannik Pulfer <jpu140816@stud.gibb.ch>
2024-09-13 11:20:20 +0200

    Implement sound feedback for magnet finder

--- fluminum/app/(tabs)/magnet-finder.tsx
+++ fluminum/app/(tabs)/magnet-finder.tsx
@@ -5,24 +5,58 @@ import { Text } from "~/components/ui/text";
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import * as Haptics from 'expo-haptics';
 import { Audio } from 'expo-av';
+import { Sound } from "expo-av/build/Audio";
+
 export default function Screen() {
   const [magnetometerValue, setMagnetometerValue] = React.useState<number>(0);
+  const [sound, setSound] = React.useState<Sound>()
+
+  async function loadSound() {
+    const asdf = await Audio.Sound.createAsync( require('../../assets/geiger-sound2.mp3'));
+    setSound(asdf.sound);
+    await asdf.sound.setIsLoopingAsync(true);
+    await asdf.sound.playAsync();
+  }
+
+  function calcSoundRate(absoluteMagnetometerValue:number) {
+    return 1 + ((4*absoluteMagnetometerValue)/1000)
+  }
 
   React.useEffect(() => {
-    let time= 0;
-    const listener = Magnetometer.addListener((e) => {
+    if (sound == undefined) return;
+
+    let time = 0;
+    let oldSoundRate = 1;
+    const listener = Magnetometer.addListener(async (e) => {
         const absoluteMagnetometerValue = Number((Math.sqrt((e.x*e.x) + (e.y*e.y) + (e.z*e.z)).toFixed(2)));
+        if (absoluteMagnetometerValue == 0) return;
         setMagnetometerValue(absoluteMagnetometerValue);
         if(Date.now() > time) {
             Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
-            time = (Date.now() + (15000/absoluteMagnetometerValue))
+            const newSoundRate = calcSoundRate(absoluteMagnetometerValue)
+            let soundRateUpdateThreshold = Math.abs((newSoundRate - oldSoundRate))
+            if(soundRateUpdateThreshold < 0 || soundRateUpdateThreshold > 32) {
+              soundRateUpdateThreshold = 1;
+            }
+            if(soundRateUpdateThreshold > 0.2) {
+              sound.setRateAsync(newSoundRate, false);
+              oldSoundRate = newSoundRate;
+            }
+            time = (Date.now() + (20000/absoluteMagnetometerValue))
         }
     });
 
     Magnetometer.setUpdateInterval(30);
 
-    return () => listener.remove();
-  }, []);
+    return () => {
+      listener.remove();
+      sound?.unloadAsync();
+    };
+  }, [sound]);
+
+  React.useEffect(() => {
+    loadSound();
+  }, [])
 
   return (
         <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
