2df42187d00ac7dc497344e6f8b094f94f323b90
Arthur Deierlein <info@c0rydoras.dev>
2024-09-13 14:16:11 +0200

    fix(link-icon): fix can't use forwardRef on function Component errors

--- fluminum/components/link-icon.tsx
+++ fluminum/components/link-icon.tsx
@@ -1,16 +1,19 @@
 import { Href, Link } from "expo-router";
 import Icon from "~/components/icon";
 import type { LucideIcon } from "lucide-react-native";
+import { TouchableOpacity } from "react-native";
 
 function LinkIcon(props: { Icon: LucideIcon; href: Href }) {
   return (
     <Link href={props.href} asChild>
-      <Icon
-        icon={props.Icon}
-        className="text-foreground"
-        size={27}
-        strokeWidth={1.25}
-      />
+      <TouchableOpacity>
+        <Icon
+          icon={props.Icon}
+          className="text-foreground"
+          size={27}
+          strokeWidth={1.25}
+        />
+      </TouchableOpacity>
     </Link>
   );
 }
