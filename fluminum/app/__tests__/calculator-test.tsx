c6e9bc2292b9ae7402f41baa569e382a4d117b7b
Arthur Deierlein <info@c0rydoras.dev>
2024-09-13 09:46:57 +0200

    chore(calculator): added more tests

--- fluminum/app/__tests__/calculator-test.tsx
+++ fluminum/app/__tests__/calculator-test.tsx
@@ -21,7 +21,7 @@ describe("calc watt", () => {
     expect(calcuateMap.watt({ volt: 100, ampere: 10, watt: null })).toBe(10);
   });
 
-  test("ampere or watt with value null returns null", () => {
+  test("ampere or volt with value null returns null", () => {
     expect(calcuateMap.watt({ volt: null, ampere: 10, watt: null })).toBe(null);
     expect(calcuateMap.watt({ volt: 100, ampere: null, watt: null })).toBe(
       null,
@@ -38,5 +38,34 @@ describe("calc watt", () => {
 
   test("volt of 0 returns 0", () => {
     expect(calcuateMap.watt({ volt: 0, ampere: 100, watt: null })).toBe(0);
+  });
+});
+
+describe("calc ampere", () => {
+  test("valid values work", () => {
+    expect(calcuateMap.ampere({ volt: 100, watt: 10, ampere: null })).toBe(0.1);
+  });
+
+  test("watt or volt with value null returns null", () => {
+    expect(calcuateMap.ampere({ volt: null, watt: 10, ampere: null })).toBe(
+      null,
+    );
+    expect(calcuateMap.ampere({ volt: 100, watt: null, ampere: null })).toBe(
+      null,
+    );
+    expect(calcuateMap.ampere({ volt: null, watt: null, ampere: null })).toBe(
+      null,
+    );
+  });
+
+  test("volt of 0 returns null", () => {
+    expect(calcuateMap.ampere({ watt: 100, volt: 0, ampere: null })).toBe(null);
+    expect(calcuateMap.ampere({ watt: 1000, volt: 0, ampere: null })).toBe(
+      null,
+    );
+  });
+
+  test("watt of 0 returns 0", () => {
+    expect(calcuateMap.ampere({ volt: 1000, watt: 0, ampere: null })).toBe(0);
   });
 });
