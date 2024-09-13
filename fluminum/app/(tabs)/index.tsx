e27f677e939f24535c58039ccf467ce4fb556244
Arthur Deierlein <info@c0rydoras.dev>
2024-09-13 14:58:08 +0200

    feat: added cable finder
    note: this find cables, its just a magnetometer used for PoC purposes

--- fluminum/app/(tabs)/index.tsx
+++ fluminum/app/(tabs)/index.tsx
@@ -1,26 +1,40 @@
 import { View } from "react-native";
 import { Zap } from "lucide-react-native";
 import Icon from "~/components/icon";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Magnetometer, MagnetometerMeasurement } from "expo-sensors";
 
 const foreground = "text-foreground fill-foreground";
 const yellow = "text-yellow-400 fill-yellow-400";
 
+const calculateAbsoluteMagnetometerValue = (data: MagnetometerMeasurement) =>
+  Math.sqrt(data.x ** 2 + data.y ** 2 + data.z ** 2);
+
 export default function Screen() {
-  const [className, setClassName] = useState<typeof foreground | typeof yellow>(
-    foreground,
+  const [absoluteMagnetometerValue, setAbsoluteMagnetometerValue] =
+    useState<number>(0);
+
+  useEffect(() => {
+    Magnetometer.setUpdateInterval(1);
+    const listener = Magnetometer.addListener((data) => {
+      setAbsoluteMagnetometerValue(calculateAbsoluteMagnetometerValue(data));
+    });
+
+    return () => {
+      listener.remove();
+    };
+  });
+
+  // NOTE: right now this doesn't actually find cables
+  // TODO: this is temporary
+  const className = useMemo(
+    () => (absoluteMagnetometerValue > 200 ? yellow : foreground),
+    [absoluteMagnetometerValue],
   );
 
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Icon
-        onPress={() =>
-          setClassName((v) => (v === yellow ? foreground : yellow))
-        }
-        icon={Zap}
-        size={200}
-        className={className}
-      />
+      <Icon icon={Zap} size={200} className={className} />
     </View>
   );
 }
