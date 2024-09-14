a7e192cbfcdf9819ad8a72cd83fd251c93962b80
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-09-14 11:23:24 +0200

    Implement dropdown for angle unit and save selection in async storage

--- fluminum/app/settings.tsx
+++ fluminum/app/settings.tsx
@@ -1,12 +1,22 @@
 import {View} from "react-native";
-import {Accordion, AccordionContent, AccordionItem, AccordionTrigger,} from "~/components/ui/accordion";
 import {Text} from "~/components/ui/text";
 import {ToggleGroup, ToggleGroupItem} from "~/components/ui/toggle-group";
 import {useEffect, useMemo, useState} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
 
 function Settings() {
     const [feedbackValue, setFeedbackValue] = useState<string[]>([]);
+    const [angleUnit, setAngleUnit] = useState<string>("rad");
     const [isLoading, setIsLoading] = useState(true);
 
     useMemo(async () => {
@@ -15,52 +25,81 @@ function Settings() {
         }
     }, [feedbackValue,isLoading]);
 
+    useMemo(async () => {
+        if(!isLoading){
+            await AsyncStorage.setItem("angleUnit", angleUnit);
+        }
+    }, [angleUnit,isLoading]);
+
     useEffect(() => {
-        AsyncStorage.getItem("feedbackValue").then((value) => {
-            if (value) {
-                console.log(value)
-                setFeedbackValue(JSON.parse(value));
+        AsyncStorage.getItem("feedbackValue").then((storedFeedbackValue) => {
+            AsyncStorage.getItem("angleUnit").then((storedAngleUnit) => {
+                storedFeedbackValue && setFeedbackValue(JSON.parse(storedFeedbackValue));
+                storedAngleUnit && setAngleUnit(storedAngleUnit);
                 setIsLoading(false);
-            }
+            })
         })
     }, [])
 
+    const insets = useSafeAreaInsets();
+    const contentInsets = {
+      top: insets.top,
+      bottom: insets.bottom,
+      left: 12,
+      right: 12,
+    };
+
+    const angleUnits: { [key:string]: string;} = {
+        "rad": "Rad",
+        "deg": "Deg",
+        "percent": "Percent"
+    }
+
     return (
       <View className="flex-1 items-center gap-5 p-6 bg-secondary/30">
-          <Accordion
-            type="multiple"
-            collapsible
-            defaultValue={["item-1"]}
-            className="w-full max-w-sm native:max-w-md"
-          >
-              <AccordionItem value="angle-unit">
-                  <AccordionTrigger>
-                      <Text>Angle Unit</Text>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                      <Text>Lorem Ipsum</Text>
-                  </AccordionContent>
-              </AccordionItem>
-          </Accordion>
-          <Text>Feedback-Arten</Text>
-                      <ToggleGroup onValueChange={setFeedbackValue} value={feedbackValue} type="multiple">
-                          <ToggleGroupItem className={feedbackValue.includes("vibration")?"bg-[#FFE500]":""} value="vibration">
-                              <Text>
-                                  Vibration
-                              </Text>
-                          </ToggleGroupItem>
-                          <ToggleGroupItem value="visual">
-                              <Text>
-                                  Lampe
-                              </Text>
-                          </ToggleGroupItem>
-                          <ToggleGroupItem value="audio">
-                              <Text>
-                                  Audio
-                              </Text>
-                          </ToggleGroupItem>
-                      </ToggleGroup>
+        <Text>Angle measurement unit</Text>
+        <Select onValueChange={(option) => setAngleUnit(option?.value || 'deg')} value={{'label': angleUnits[angleUnit], value: angleUnit}}>
+            <SelectTrigger className='w-[250px]'>
+                <SelectValue
+                    className='text-foreground text-sm native:text-lg'
+                    placeholder='Select an angle unit'
+                />
+            </SelectTrigger>
+            <SelectContent insets={contentInsets} className='w-[250px]'>
+                <SelectGroup>
+                    <SelectLabel>Angle units</SelectLabel>
+                    <SelectItem label='Rad' value='rad'>
+                        Rad
+                    </SelectItem>
+                    <SelectItem label='Deg' value='deg'>
+                        Deg
+                    </SelectItem>
+                    <SelectItem label='Percent' value='percent'>
+                        Percent
+                    </SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
 
+
+        <Text>Feedback-Arten</Text>
+        <ToggleGroup onValueChange={setFeedbackValue} value={feedbackValue} type="multiple">
+            <ToggleGroupItem className={feedbackValue.includes("vibration")?"bg-[#FFE500]":""} value="vibration">
+                <Text>
+                    Vibration
+                </Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="visual">
+                <Text>
+                    Lampe
+                </Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="audio">
+                <Text>
+                    Audio
+                </Text>
+            </ToggleGroupItem>
+        </ToggleGroup>
       </View>
     );
 }
