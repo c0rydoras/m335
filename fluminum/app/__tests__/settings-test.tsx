befc11460928635bdb9e2d3c4f97d295ef923042
Jannik Pulfer <jpu140816@stud.gibb.ch>
2024-09-16 08:34:01 +0200

    Implement snapshot test

--- fluminum/app/__tests__/settings-test.tsx
+++ fluminum/app/__tests__/settings-test.tsx
@@ -1,29 +1,27 @@
-import { SelectGroup } from "~/components/ui/select";
-import { SelectContent } from "~/components/ui/select";
-import { SelectLabel } from "~/components/ui/select";
-import { SelectItem } from "~/components/ui/select";
 import { render } from "@testing-library/react-native";
-import { Select } from "~/components/ui/select";
+import { ToggleGroup } from "~/components/ui/toggle-group";
+import { ToggleGroupItem } from "~/components/ui/toggle-group";
+import { Text } from "~/components/ui/text";
 
-test("<Settings />", () => {
+test("Toggle group", () => {
     const tree = render(
-        <Select>
-            <SelectContent className='w-[250px]'>
-                <SelectGroup>
-                    <SelectLabel>Angle units</SelectLabel>
-                    <SelectItem label='Deg' value='deg'>
-                        Deg
-                    </SelectItem>
-                    <SelectItem label='Rad' value='rad'>
-                        Rad
-                    </SelectItem>
-                    <SelectItem label='Percent' value='percent'>
-                        Percent
-                    </SelectItem>
-                </SelectGroup>
-            </SelectContent>
-        </Select>
-
+        <ToggleGroup onValueChange={() => {}} value='deg' type="single">
+            <ToggleGroupItem value="deg">
+                <Text>
+                    Deg
+                </Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="rad">
+                <Text>
+                    Rad
+                </Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="percent">
+                <Text>
+                    Percent
+                </Text>
+            </ToggleGroupItem>
+        </ToggleGroup>
     );
     expect(tree).toMatchSnapshot();
   });
