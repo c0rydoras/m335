c5067b1e332e6c1309603ea54bb2a29ec1effee5
Arthur Deierlein <info@c0rydoras.dev>
2024-09-16 10:38:40 +0200

    feat: notifications
    added disclaimer as specified in mockup

--- fluminum/app/(tabs)/index.tsx
+++ fluminum/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { View } from "react-native";
 import { Zap } from "lucide-react-native";
+import { Text } from "~/components/ui/text";
 import Icon from "~/components/icon";
 import { useCallback, useState } from "react";
 import { Magnetometer, MagnetometerMeasurement } from "expo-sensors";
 import { useFocusEffect } from "expo-router";
+import { Badge } from "~/components/ui/badge";
 
 const foreground = "text-foreground fill-foreground";
 const yellow = "text-yellow-400 fill-yellow-400";
@@ -30,12 +32,21 @@ export default function Screen() {
   // NOTE: right now this doesn't actually find cables
   // TODO: this is temporary
   return (
-    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Icon
-        icon={Zap}
-        size={200}
-        className={absoluteMagnetometerValue > 200 ? yellow : foreground}
-      />
+    <View className="flex justify-center items-center gap-5 p-6 bg-secondary/3">
+      <View className="h-[25%]">
+        <Badge variant="destructive" className="rounded-md p-4">
+          <Text className="text-2xl text-center">
+            Es können nicht alle Stromführende Leitungen entdeckt werden!
+          </Text>
+        </Badge>
+      </View>
+      <View className="h-[75%]">
+        <Icon
+          icon={Zap}
+          size={300}
+          className={absoluteMagnetometerValue > 200 ? yellow : foreground}
+        />
+      </View>
     </View>
   );
 }
